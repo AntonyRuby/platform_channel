4a4360ac8034671e76706b639dd85d17b07e857a
Antony Ruby Alexia <alexia@m2p.in>
2022-01-05 18:52:24 +0530

    encryption and decryption

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import Foundation
+import CryptoSwift
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,25 +12,80 @@ import Flutter
       
       let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
 
-      let CHANNEL = FlutterMethodChannel(name: "com.example.flutter/device_info", binaryMessenger: controller.binaryMessenger)
+      let platform = FlutterMethodChannel(name: "com.example.flutter/device_info", binaryMessenger: controller.binaryMessenger)
 
-      CHANNEL.setMethodCallHandler { (methodCall, result)in
-
-      if methodCall.method == "Print"{
-
-      result("Hi")
-
-      }
-
-      }
+      platform.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+//          if call.method == "getDeviceInfo" {
+//                     self?.deviceInfo(result: result)
+//                 }
+//         else
+             if(call.method == "encrypt"){
+                guard let args = call.arguments as? [String : Any] else {return}
+                let data = args["data"] as! String
+                let key = args["key"] as! String
+                let encryptedString = CryptoHelper.encrypt(dataFromFlutter: data, keyFromFlutter: key)
+                self?.encrypt(result: result, encrypted: encryptedString!)
+                return
+            }else if(call.method == "decrypt"){
+                             guard let args = call.arguments as? [String : Any] else {return}
+                             let data = args["data"] as! String
+                             let key = args["key"] as! String
+                             let decryptedString = CryptoHelper.decrypt(dataFromFlutter: data, keyFromFlutter: key)
+                             self?.decrypt(result: result, decrypted: decryptedString!)
+                             return
+                         }else {
+                             result(FlutterMethodNotImplemented)
+                             return
+                           }
+             
+             
+          
+      });
 
 //      GeneratedPluginRegistrant.register(with: self)
 
       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
 
-
-
       }
+    
+    private func encrypt(result: FlutterResult, encrypted: String) {
+            result(encrypted)
+        }
 
-      }
-      
+        private func decrypt(result: FlutterResult, decrypted: String) {
+            result(decrypted)
+        }
+}
+
+var keyValue :String!
+
+class CryptoHelper{
+    private static let iv = "hertllertoer";
+    // ENCRYPTION
+    public static func encrypt(dataFromFlutter :String, keyFromFlutter :String) -> String? {
+        
+        
+        print(dataFromFlutter);
+        print(keyFromFlutter);
+        
+        do{
+            let encrypted: Array<UInt8> = try AES(key: keyFromFlutter, iv: iv, padding: .pkcs5).encrypt(Array(dataFromFlutter.utf8))
+            return encrypted.toBase64()
+        }catch{
+            return "DATA ERROR"
+        }
+    }
+
+    // DECRYPTION
+    public static func decrypt(dataFromFlutter :String, keyFromFlutter :String) -> String? {
+        do{
+            let data = Data(base64Encoded: dataFromFlutter)
+            let decrypted = try AES(key: keyFromFlutter, iv: iv, padding: .pkcs5).decrypt(data!.bytes)
+            return String(data: Data(decrypted), encoding: .utf8)
+        }catch{
+            return "DATA ERROR"
+        }
+    }
+}
+
